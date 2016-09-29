85fb2438a83d53a03ea326e4e54ac874fb91c748
Stephen Wickwire <wickwirest@mullenmail.com>
2016-09-29 10:24:05 -0600

    Fix the ai move and try to start the win conditions

--- Array Project/src/ArrayRunner.java
+++ Array Project/src/ArrayRunner.java
@@ -8,6 +8,7 @@ public class ArrayRunner
 		static String aiCharacter;
 		static boolean game;
 		static String playerMove;
+		static boolean gameContinues;
 		public static void main(String[] args)
 			{
 				greetUser();
@@ -15,13 +16,16 @@ public class ArrayRunner
 				MakeBoard.main(args);
 				pickCharacter();
 					
-				CheckUnderneath.main(args);
+				//CheckUnderneath.main(args);
+				while(gameContinues)
+					{
 				makeMove();
-				aiMove();
-				
+				MoveUnderneath.main(args);
+					}
 			}
 		public static void greetUser()
 			{
+				gameContinues =true;
 				System.out.println("Hi, Whats your name?");
 				//fixed minor grammar error^^^
 				Scanner sc = new Scanner(System.in);
@@ -95,151 +99,379 @@ public class ArrayRunner
 				case "A1":
 				case "a1":
 						{
-							MakeBoard.connectBoard[0][0]= character;
+							if(!MakeBoard.connectBoard[0][0].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[0][0]= character;
+								
+							}
 							break;
 						}
 				case "A2":
 				case "a2":
 						{
-							MakeBoard.connectBoard[0][1]= character;
+							if(!MakeBoard.connectBoard[0][1].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[0][1]= character;
+								
+							}
 							break;
 						}
 				case "A3":
 				case "a3":
 						{
-							MakeBoard.connectBoard[0][2]= character;
+							if(!MakeBoard.connectBoard[0][2].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[0][2]= character;
+								
+							}
 							break;
 						}
 				case "A4":
 				case "a4":
 						{
-							MakeBoard.connectBoard[0][3]= character;
+							if(!MakeBoard.connectBoard[0][3].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[0][3]= character;
+								
+							}
 							break;
 						}
 				case "A5":
 				case "a5":
 						{
-							MakeBoard.connectBoard[0][4]= character;
+							if(!MakeBoard.connectBoard[0][4].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[0][4]= character;
+								
+							}
 							break;
 						}
 				case "B1":
 				case "b1":
 						{
-							MakeBoard.connectBoard[1][0]= character;
+							if(!MakeBoard.connectBoard[1][0].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[1][0]= character;
+								
+							}
 							break;
 						}
 				case "B2":
 				case "b2":
 						{
-							MakeBoard.connectBoard[1][1]= character;
+							if(!MakeBoard.connectBoard[1][1].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[1][1]= character;
+								
+							}
 							break;
 						}
 				case "B3":
 				case "b3":
 						{
-							MakeBoard.connectBoard[1][2]= character;
+							if(!MakeBoard.connectBoard[1][2].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[1][2]= character;
+								
+							}
 							break;
 						}
 				case "B4":
 				case "b4":
 						{
-							MakeBoard.connectBoard[1][3]= character;
+							if(!MakeBoard.connectBoard[1][3].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[1][3]= character;
+								
+							}
 							break;
 						}
 				case "B5":
 				case "b5":
 						{
-							MakeBoard.connectBoard[1][4]= character;
+							if(!MakeBoard.connectBoard[1][4].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[1][4]= character;
+								
+							}
 							break;
+							
 						}
 				case "C1":
 				case "c1":
 						{
-							MakeBoard.connectBoard[2][0]= character;
+							if(!MakeBoard.connectBoard[2][0].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[2][0]= character;
+							
+							}
 							break;
+							
 						}
 				case "C2":
 				case "c2":
 						{
-							MakeBoard.connectBoard[2][1]= character;
+							if(!MakeBoard.connectBoard[2][1].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[2][1]= character;
+								
+							}
 							break;
 						}
 				case "C3":
 				case "c3":
 						{
-							MakeBoard.connectBoard[2][2]= character;
+							if(!MakeBoard.connectBoard[2][2].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[2][2]= character;
+								
+							}
 							break;
 						}
 				case "C4":
 				case "c4":
 						{
-							MakeBoard.connectBoard[2][3]= character;
+							if(!MakeBoard.connectBoard[2][3].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[2][3]= character;
+								
+							}
+							
 							break;
 						}
 				case "C5":
 				case "c5":
 						{
-							MakeBoard.connectBoard[2][4]= character;
+							if(!MakeBoard.connectBoard[2][4].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[2][4]= character;
+								
+							}
 							break;
 						}
 				case "D1":
 				case "d1":
 						{
-							MakeBoard.connectBoard[3][0]= character;
+							if(!MakeBoard.connectBoard[3][0].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[3][0]= character;
+								
+							}
 							break;
 						}
 				case "D2":
 				case "d2":
 						{
-							MakeBoard.connectBoard[3][1]= character;
+							if(!MakeBoard.connectBoard[3][1].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[3][1]= character;
+								
+							}
 							break;
 						}
 				case "D3":
 				case "d3":
 						{
-							MakeBoard.connectBoard[3][2]= character;
+							if(!MakeBoard.connectBoard[3][2].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[3][2]= character;
+								
+							}
 							break;
 						}
 				case "D4":
 				case "d4":
 						{
-							MakeBoard.connectBoard[3][3]= character;
+							if(!MakeBoard.connectBoard[3][3].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[3][3]= character;
+								
+							}
 							break;
 						}
 				case "D5":
 				case "d5":
 						{
-							MakeBoard.connectBoard[3][4]= character;
+							if(!MakeBoard.connectBoard[3][4].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[3][4]= character;
+								
+							}
 							break;
 						}
 				case "E1":
 				case "e1":
 						{
-							MakeBoard.connectBoard[4][0]= character;
+							if(!MakeBoard.connectBoard[4][0].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[4][0]= character;
+								
+							}
 							break;
 						}
 				case "E2":
 				case "e2":
 						{
-							MakeBoard.connectBoard[4][1]= character;
+							if(!MakeBoard.connectBoard[4][1].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[4][1]= character;
+								
+							}
 							break;
 						}
 				case "E3":
 				case "e3":
 						{
-							MakeBoard.connectBoard[4][2]= character;
+							if(!MakeBoard.connectBoard[4][2].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[4][2]= character;
+								
+							}
 							break;
 						}
 				case "E4":
 				case "e4":
 						{
-							MakeBoard.connectBoard[4][3]= character;
+							if(!MakeBoard.connectBoard[4][3].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[4][3]= character;
+								
+							}
 							break;
 						}
 				case "E5":
 				case "e5":
 						{
-							MakeBoard.connectBoard[4][4]= character;
+							if(!MakeBoard.connectBoard[4][4].equals(""))
+								{
+									System.out.println("Invalid Move!");
+									makeMove();
+								}
+							else
+							{
+								MakeBoard.connectBoard[4][4]= character;
+								
+							}
 							break;
 						}
 			}
